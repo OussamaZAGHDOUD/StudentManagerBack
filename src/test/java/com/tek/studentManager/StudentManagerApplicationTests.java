c7d239ceb9c2a8f433fe29db9d6bc88de8a55503
oussamaZAGHDOUD <oussamazaghdoud1991@gmail.com>
2021-04-18 02:01:49 +0100

    empty project

---
+++ src/test/java/com/tek/studentManager/StudentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tek.studentManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
