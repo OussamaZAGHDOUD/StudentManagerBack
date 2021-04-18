9c592d066abcce15b315990f022b702415666a42
oussamaZAGHDOUD <oussamazaghdoud1991@gmail.com>
2021-04-18 11:25:02 +0100

    sql file to initialise the DB

---
+++ src/main/resources/data-mysql.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `student` (`id`, `class_name`, `email`, `image_url`, `name`, `phone`, `student_code`) VALUES
+(1, 'GLSI', 'oussama@gmail.com', 'https://www.bootdey.com/img/Content/avatar/avatar1.png', 'oussama', '27652211', 'a745a3b5-e7b2-46c9-a0c5-ae8a45be8f5e'),
+(2, 'TIC', 'Amine@mail.com', 'https://www.bootdey.com/img/Content/avatar/avatar2.png', 'Amine', '50555666', 'a744a3b5-e7b2-46c9-a0c5-ae8a45be1f5e'),
+(3, 'GL', 'Sami@gmail.com', 'https://www.bootdey.com/img/Content/avatar/avatar2.png', 'Sami', '99779088', '990fe510-f359-4706-9d2c-c97764716d02'),
+(4, 'Science', 'amel@gmail.com', 'https://www.bootdey.com/img/Content/avatar/avatar3.png', 'Amal', '94749088', '4042a4cc-9779-4fcf-b86e-08e57bd09334');
